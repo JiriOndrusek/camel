057186fb5951bb143b48a79dfe7cd1315899eb8b
JiriOndrusek <jondruse@redhat.com>
2019-03-01 15:22:37 +0100

    linkedin-refactor-to-jsoup

--- components/camel-linkedin/camel-linkedin-api/src/main/java/org/apache/camel/component/linkedin/api/LinkedInOAuthRequestFilter.java
+++ components/camel-linkedin/camel-linkedin-api/src/main/java/org/apache/camel/component/linkedin/api/LinkedInOAuthRequestFilter.java
@@ -17,6 +17,7 @@
 package org.apache.camel.component.linkedin.api;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -34,29 +35,16 @@ import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.ext.Provider;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.HttpMethod;
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.ProxyConfig;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.WebClientOptions;
-import com.gargoylesoftware.htmlunit.WebRequest;
-import com.gargoylesoftware.htmlunit.WebResponse;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
-import com.gargoylesoftware.htmlunit.html.HtmlDivision;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
-import com.gargoylesoftware.htmlunit.util.WebConnectionWrapper;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpStatus;
-import org.apache.http.conn.params.ConnRoutePNames;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import test.LinkedInJSoupTest;
 
 /**
  * LinkedIn OAuth request filter to handle OAuth token.
@@ -64,6 +52,11 @@ import org.slf4j.LoggerFactory;
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
+
+    private static final int SC_OK = 200;
+    private static final int SC_MOVED_TEMPORARILY = 302;
+    private static final int SC_SEE_OTHER = 303;
+    private static final String HEADER_LOCATION = "location";
 
     public static final String BASE_ADDRESS = "https://api.linkedin.com/v1";
 
@@ -81,8 +74,6 @@ public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
 
     private static final Pattern QUERY_PARAM_PATTERN = Pattern.compile("&?([^=]+)=([^&]+)");
 
-    private final WebClient webClient;
-
     private final OAuthParams oAuthParams;
 
     private OAuthToken oAuthToken;
@@ -90,36 +81,9 @@ public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
     public LinkedInOAuthRequestFilter(OAuthParams oAuthParams, Map<String, Object> httpParams,
                                       boolean lazyAuth, String[] enabledProtocols) {
 
+        //TODO JiriOndrusek test http proxy
         this.oAuthParams = oAuthParams;
         this.oAuthToken = null;
-
-        // create HtmlUnit client
-        webClient = new WebClient(BrowserVersion.FIREFOX_38);
-        final WebClientOptions options = webClient.getOptions();
-        options.setRedirectEnabled(true);
-        options.setJavaScriptEnabled(false);
-        options.setThrowExceptionOnFailingStatusCode(true);
-        options.setThrowExceptionOnScriptError(true);
-        options.setPrintContentOnFailingStatusCode(LOG.isDebugEnabled());
-        options.setSSLClientProtocols(enabledProtocols);
-
-        // add HTTP proxy if set
-        if (httpParams != null && httpParams.get(ConnRoutePNames.DEFAULT_PROXY) != null) {
-            final HttpHost proxyHost = (HttpHost) httpParams.get(ConnRoutePNames.DEFAULT_PROXY);
-            final Boolean socksProxy = (Boolean) httpParams.get("http.route.socks-proxy");
-            final ProxyConfig proxyConfig = new ProxyConfig(proxyHost.getHostName(), proxyHost.getPort(),
-                socksProxy != null ? socksProxy : false);
-            options.setProxyConfig(proxyConfig);
-        }
-
-        // disable default gzip compression, as error pages are sent with no compression and htmlunit doesn't negotiate
-        new WebConnectionWrapper(webClient) {
-            @Override
-            public WebResponse getResponse(WebRequest request) throws IOException {
-                request.setAdditionalHeader(HttpHeaders.ACCEPT_ENCODING, "identity");
-                return super.getResponse(request);
-            }
-        };
 
         if (!lazyAuth) {
             try {
@@ -133,112 +97,46 @@ public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
 
     @SuppressWarnings("deprecation")
     private String getRefreshToken() {
-        // disable redirect to avoid loading error redirect URL
-        webClient.getOptions().setRedirectEnabled(false);
 
         try {
-            final String csrfId = String.valueOf(new SecureRandom().nextLong());
 
+            final String csrfId = String.valueOf(new SecureRandom().nextLong());
             final String encodedRedirectUri = URLEncoder.encode(oAuthParams.getRedirectUri(), "UTF-8");
             final OAuthScope[] scopes = oAuthParams.getScopes();
 
-            final String url;
-            if (scopes == null || scopes.length == 0) {
-                url = String.format(AUTHORIZATION_URL, oAuthParams.getClientId(),
-                    csrfId, encodedRedirectUri);
-            } else {
-                final int nScopes = scopes.length;
-                final StringBuilder builder = new StringBuilder();
-                int i = 0;
-                for (OAuthScope scope : scopes) {
-                    builder.append(scope.getValue());
-                    if (++i < nScopes) {
-                        builder.append("%20");
-                    }
-                }
-                url = String.format(AUTHORIZATION_URL_WITH_SCOPE, oAuthParams.getClientId(), csrfId,
-                    builder.toString(), encodedRedirectUri);
-            }
-            HtmlPage authPage = null;
-            try {
-                authPage = webClient.getPage(url);
-            } catch (FailingHttpStatusCodeException e) {
-                // only handle errors returned with redirects
-                boolean done = false;
-                do {
-                    if (e.getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY || e.getStatusCode() == HttpStatus.SC_SEE_OTHER) {
-                        final URL location = new URL(e.getResponse().getResponseHeaderValue(HttpHeaders.LOCATION));
-                        final String locationQuery = location.getQuery();
-                        if (locationQuery != null && locationQuery.contains("error=")) {
-                            throw new IOException(URLDecoder.decode(locationQuery).replaceAll("&", ", "));
-                        } else {
-                            // follow the redirect to login form
-                            try {
-                                authPage = webClient.getPage(location);
-                                done = true;
-                            } catch (FailingHttpStatusCodeException e1) {
-                                e = e1;
-                            }
-                        }
-                    } else {
-                        throw e;
-                    }
-                } while (!done);
-            }
-            // look for <div role="alert">
-            final HtmlDivision div = authPage.getFirstByXPath("//div[@role='alert'][not(contains(@class,'hidden'))]");
-            if (div != null) {
-                throw new IllegalArgumentException("Error authorizing application: " + div.getTextContent());
+            final String url = initializeUrl(csrfId, encodedRedirectUri, scopes);
+
+            //load loginPage
+            final Connection.Response loginPageResponse = getLoginResponse(url);
+            final Document loginPage = loginPageResponse.parse(); // this is the document that contains response html
+
+            //validate for loginErrors
+            validatePageErrors(loginPage);
+
+            //fill login form
+            FormElement loginForm = (FormElement)loginPage.select("form").first();
+
+            Element loginField = loginForm.select("input[name=session_key]").first();
+            loginField.val(oAuthParams.getUserName());
+
+            Element passwordField = loginForm.select("input[name=session_password]").first();
+            passwordField.val(oAuthParams.getUserPassword());
+
+            //submit loginPage
+            Map<String, String> cookies = new HashMap();
+            cookies.putAll(loginPageResponse.cookies());
+
+            ResponseWithQuery response = submitFormAndRedirect(cookies, loginForm);
+
+            //if needed allow acess
+            if(response.getResponse() != null) {
+                Document allowPage = response.getResponse().parse();
+                FormElement allowForm = (FormElement)allowPage.select("form").get(1);
+                response = submitFormAndRedirect(cookies, allowForm);
             }
 
-            // submit login credentials
-            final HtmlForm loginForm = authPage.getForms().get(0);
-            final HtmlTextInput login = loginForm.getInputByName("session_key");
-            login.setText(oAuthParams.getUserName());
-            final HtmlPasswordInput password = loginForm.getInputByName("session_password");
-            password.setText(oAuthParams.getUserPassword());
-            final HtmlButton submitInput = (HtmlButton) loginForm.getElementsByAttribute("button", "type", "submit").get(0);
-
-            // validate CSRF and get authorization code
-            String nextLocation = null;
-            try {
-                final Page redirectPage = submitInput.click();
-            } catch (FailingHttpStatusCodeException e) {
-
-                // escalate non redirect errors
-                if (e.getStatusCode() != HttpStatus.SC_MOVED_TEMPORARILY && e.getStatusCode() != HttpStatus.SC_SEE_OTHER) {
-                    throw e;
-                }
-                //redirect from auth page leads to the new page with acceptance of access
-                nextLocation = AUTHORIZATION_URL_PREFIX + e.getResponse().getResponseHeaderValue(HttpHeaders.LOCATION);
-            }
-            if (nextLocation == null) {
-                throw new IllegalArgumentException("Redirect response query is null, check username, password and permissions");
-            }
-
-            //Allow access page
-            HtmlPage allowAccessPage = null;
-            String redirectQuery;
-            try {
-                //if access is already allowed, redirection exception is thrown
-                allowAccessPage = webClient.getPage(nextLocation);
-                //in other cases, allow button has to be used
-                final HtmlForm redirectForm = allowAccessPage.getForms().get(1);
-                final HtmlButton allowButton = (HtmlButton) redirectForm.getElementsByAttribute("button", "type", "submit").get(0);
-                final Page redirectPage = allowButton.click();
-                redirectQuery = redirectPage.getUrl().getQuery();
-            } catch (FailingHttpStatusCodeException e) {
-                // escalate non redirect errors
-                if (e.getStatusCode() != HttpStatus.SC_MOVED_TEMPORARILY && e.getStatusCode() != HttpStatus.SC_SEE_OTHER) {
-                    throw e;
-                }
-                redirectQuery = new URL(e.getResponse().getResponseHeaderValue(HttpHeaders.LOCATION)).getQuery();
-            }
-            if (redirectQuery == null) {
-                throw new IllegalArgumentException("Redirect response query is null, check username, password and permissions");
-            }
-            final Map<String, String> params = new HashMap<>();
-            final Matcher matcher = QUERY_PARAM_PATTERN.matcher(redirectQuery);
+            final Map<String, String> params = new HashMap<String, String>();
+            final Matcher matcher = QUERY_PARAM_PATTERN.matcher(response.getRedirectQuerty());
             while (matcher.find()) {
                 params.put(matcher.group(1), matcher.group(2));
             }
@@ -254,29 +152,126 @@ public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
                 // TODO check results??
                 return params.get("code");
             }
-
         } catch (Exception e) {
             throw new IllegalArgumentException("Error authorizing application: " + e.getMessage(), e);
         }
     }
 
-    public void close() {
-        webClient.close();
+    private ResponseWithQuery submitFormAndRedirect(Map<String, String> cookies, FormElement loginForm) throws IOException {
+        Connection.Response response;
+
+        int statusCode;
+        String redirectUrl = null;
+        FormElement form = loginForm;
+        do {
+            Connection con = redirectUrl == null ? form.submit() :  Jsoup.connect(redirectUrl);
+            //clear tmp values
+            form = null;
+            redirectUrl = null;
+
+
+            response = con
+                    .followRedirects(false)
+                    .cookies(cookies)
+                    .execute();
+
+            statusCode = response.statusCode();
+            cookies.putAll(response.cookies());
+
+            switch (statusCode) {
+                case SC_OK:
+                    Document page = response.parse();
+                    validatePageErrors(page);
+                    break;
+                case SC_MOVED_TEMPORARILY:
+                case SC_SEE_OTHER:
+                    String headerLocation = response.header(HEADER_LOCATION);
+                    URL location;
+                    try {
+                        location = new URL(headerLocation);
+                        //in case that url is complete, we can skip following code, because it means that it is redirected to final page
+                        //TODO JiriOndrusek test with reditect url linkedin.com
+                        return new ResponseWithQuery(null, location.getQuery());
+                    } catch (MalformedURLException e) {
+                        //in case of malformed url, it misses prefix
+                        location = new URL(AUTHORIZATION_URL_PREFIX+headerLocation);
+                    }
+                    final String locationQuery = location.getQuery();
+                    if (locationQuery != null && locationQuery.contains("error=")) {
+                        throw new IOException(URLDecoder.decode(locationQuery).replaceAll("&", ", "));
+                    } else {
+                        // follow the redirect to login form
+                        redirectUrl = location.toString();
+                    }
+
+                    break;
+                default:
+                    throw new IllegalArgumentException(String.format("Can not loade page '%s' (status code '%s')", response.url().toString(), response.statusCode()));
+            }
+
+        } while(SC_MOVED_TEMPORARILY == statusCode || SC_SEE_OTHER == statusCode);
+
+        return new ResponseWithQuery(response, null);
+    }
+
+    private void validatePageErrors(Document loginPage) {
+        Elements errorDivs = loginPage.select("body[class=error]");
+        if(errorDivs.isEmpty()) {
+            errorDivs = loginPage.select("div[role=alert]:not([class*=hidden])");
+        }
+
+        if (!errorDivs.isEmpty()) {
+            final String errorMessage = errorDivs.first().text();
+            throw new IllegalArgumentException("Error authorizing application: " + errorMessage);
+        }
+    }
+
+    private Connection.Response getLoginResponse(String url) throws IOException {
+
+        Connection.Response loginPageResponse = Jsoup.connect(url).followRedirects(false).method(Connection.Method.GET).execute();
+        // only handle errors returned with redirects
+        if (loginPageResponse.statusCode() == SC_MOVED_TEMPORARILY || loginPageResponse.statusCode() == SC_SEE_OTHER) {
+            final URL location = new URL(loginPageResponse.header(HEADER_LOCATION));
+            final String locationQuery = location.getQuery();
+            if (locationQuery != null && locationQuery.contains("error=")) {
+                throw new IOException(URLDecoder.decode(locationQuery).replaceAll("&", ", "));
+            } else {
+                // follow the redirect to login form
+                loginPageResponse = Jsoup.connect(location.toString()).followRedirects(false).method(Connection.Method.GET).execute();
+            }
+        }
+        return loginPageResponse;
+    }
+
+    private String initializeUrl(String csrfId, String encodedRedirectUri, OAuthScope[] scopes) {
+        String url;
+        if (scopes == null || scopes.length == 0) {
+            url = String.format(AUTHORIZATION_URL, oAuthParams.getClientId(), csrfId, encodedRedirectUri);
+        } else {
+            final int nScopes = scopes.length;
+            final StringBuilder builder = new StringBuilder();
+            int i = 0;
+            for (OAuthScope scope : scopes) {
+                builder.append(scope.getValue());
+                if (++i < nScopes) {
+                    builder.append("%20");
+                }
+            }
+            url = String.format(AUTHORIZATION_URL_WITH_SCOPE, oAuthParams.getClientId(), csrfId, builder.toString(), encodedRedirectUri);
+        }
+        return url;
     }
 
     private OAuthToken getAccessToken(String refreshToken) throws IOException {
-        final String tokenUrl = String.format(ACCESS_TOKEN_URL, refreshToken,
-            oAuthParams.getRedirectUri(), oAuthParams.getClientId(), oAuthParams.getClientSecret());
-        final WebRequest webRequest = new WebRequest(new URL(tokenUrl), HttpMethod.POST);
+        final String tokenUrl = String.format(ACCESS_TOKEN_URL, refreshToken, oAuthParams.getRedirectUri(), oAuthParams.getClientId(), oAuthParams.getClientSecret());
+        final Connection.Response response = Jsoup.connect(tokenUrl).ignoreContentType(true).method(Connection.Method.POST).execute();
 
-        final WebResponse webResponse = webClient.loadWebResponse(webRequest);
-        if (webResponse.getStatusCode() != HttpStatus.SC_OK) {
-            throw new IOException(String.format("Error getting access token: [%s: %s]",
-                webResponse.getStatusCode(), webResponse.getStatusMessage()));
+        if (response.statusCode() != SC_OK) {
+            throw new IOException(String.format("Error getting access token: [%s]", response.statusCode()));
         }
         final long currentTime = System.currentTimeMillis();
         final ObjectMapper mapper = new ObjectMapper();
-        final Map map = mapper.readValue(webResponse.getContentAsStream(), Map.class);
+        final Map map = mapper.readValue(response.body(), Map.class);
         final String accessToken = map.get("access_token").toString();
         final Integer expiresIn = Integer.valueOf(map.get("expires_in").toString());
         return new OAuthToken(refreshToken, accessToken,
@@ -337,5 +332,28 @@ public final class LinkedInOAuthRequestFilter implements ClientRequestFilter {
                 secureStorage.saveOAuthToken(this.oAuthToken);
             }
         }
+    }
+
+    /**
+     * Helper class, which contains 2 return values needed inside method getRefreshToken.
+     */
+    class ResponseWithQuery {
+        final Connection.Response response;
+        final String redirectQuery;
+
+        public ResponseWithQuery(Connection.Response response, String redirectQuery) {
+            this.response = response;
+            this.redirectQuery = redirectQuery;
+        }
+
+        public Connection.Response getResponse() {
+            return response;
+        }
+
+        public String getRedirectQuerty() {
+            return redirectQuery;
+        }
+
+
     }
 }
