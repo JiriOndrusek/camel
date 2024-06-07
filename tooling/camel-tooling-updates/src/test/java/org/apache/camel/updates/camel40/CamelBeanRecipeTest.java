e0e980046a3ec1c564849978dfc899daf72533e1
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-06-07 10:29:17 +0200

    used target

--- tooling/camel-tooling-updates/src/test/java/org/apache/camel/updates/camel40/CamelBeanRecipeTest.java
+++ tooling/camel-tooling-updates/src/test/java/org/apache/camel/updates/camel40/CamelBeanRecipeTest.java
@@ -18,7 +18,6 @@ package org.apache.camel.updates.camel40;
 
 import org.apache.camel.updates.CamelQuarkusTestUtil;
 import org.junit.jupiter.api.Test;
-import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.TypeValidation;
@@ -30,8 +29,8 @@ public class CamelBeanRecipeTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         CamelQuarkusTestUtil.recipe(spec, CamelQuarkusTestUtil.CamelVersion.v4_0)
-                .parser(JavaParser.fromJavaVersion().logCompilationWarningsAndErrors(true).classpath("camel-bean",
-                        "camel-core-model"))
+                .parser(CamelQuarkusTestUtil.parserFromClasspath(CamelQuarkusTestUtil.CamelVersion.v3_18,
+                        "camel-bean", "camel-core-model"))
                 .typeValidationOptions(TypeValidation.none());
     }
 
